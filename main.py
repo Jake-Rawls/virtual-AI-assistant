1f925473bfdb253ca0d89e0c6ca7d9c0abaf89b9
Jake Rawls <127891904+Jake-Rawls@users.noreply.github.com>
2023-06-13 11:32:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
 from roku import Roku
 
-roku = Roku('192.168.1.218')
+# Don't forget to replace the X's with your roku TV's IP address.
+# Example -> roku = Roku('192.168.1.50')
+roku = Roku('xxx.xxx.xxx.xxx')
 
 r = sr.Recognizer()
 
@@ -10,40 +12,56 @@ def process_command(command):
 
     if 'power on' in command:
         roku.poweron()
+        
     elif 'power off' in command:
         roku.poweroff()
+        
     elif 'home' in command:
         roku.home()
+        
     elif 'volume up' in command:
         roku.volume_up()
+        
     elif 'volume down' in command:
         roku.volume_down()
+        
     elif 'mute' in command:
         roku.volume_mute()
+        
     elif 'unmute' in command:
         roku.volume_mute()
+        
     elif 'up' in command:
         roku.up()
+        
     elif 'down' in command:
         roku.down()
+        
     elif 'left' in command:
         roku.left()
+        
     elif 'right' in command:
         roku.right()
+        
     elif 'select' in command:
         roku.select()
+        
     elif 'netflix' in command:
         app = roku['Netflix']
         app.launch()
+        
     elif 'peacock' in command:
         app = roku['593099']
         app.launch()
+        
     elif 'hulu' in command:
         app = roku['2285']
         app.launch()
+        
     elif 'xbox' in command:
         app = roku['tvinput.hdmi1']
         app.launch()
+        
     else:
         print("Command not recognized.")
 
@@ -55,5 +73,6 @@ while True:
         try:
             command = r.recognize_google(audio)
             process_command(command)
+            
         except sr.UnknownValueError:
             pass
